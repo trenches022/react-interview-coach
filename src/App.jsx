ef15f0e9f65006fd8c9d77bde678bd51e6a9ad61
./trenches <romanvagner@mail.com>
2025-02-16 22:06:02 +0100

    Added middle and senior paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import Frontend from './pages/Frontend/Frontend';
 import FrontendJunior from './pages/Frontend/FrontendJunior';
+import FrontendMiddle from './pages/Frontend/FrontendMiddle';
+import FrontendSenior from './pages/Frontend/FrontendSenior';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Route path='/' element={<MainPage />}/>
           <Route path='/frontend' element={<Frontend />}/>
           <Route path='/frontend/junior' element={<FrontendJunior />}/>
+          <Route path='/frontend/middle' element={<FrontendMiddle />}/>
+          <Route path='/frontend/senior' element={<FrontendSenior />}/>
         </Routes>
       </Router>
     </>
